9b989b71ee8f405459d50a72aa146792d7c5a1cc
charly2790 <carlosba90@gmail.com>
2023-08-17 02:09:20 -0300

    Owners: error en edit registro. Se requiere correccion

--- controllers/owners.js
+++ controllers/owners.js
@@ -1,7 +1,8 @@
 const { json } = require('body-parser');
+const { Op } = require('sequelize');
 const Owner = require('../models').Owner;
 
-module.exports = {  
+module.exports = {
     index: function(req,res){
         Owner.findAll().then((owners)=>{
             console.log("La cantidad de owners es: ",owners.length);
@@ -25,5 +26,41 @@ module.exports = {
             //Si hay error lo mostramos
             res.json(err);
         });
-    }
+    },
+    search: function(req,res){
+        res.render('owners/search');
+    },
+    find: function(req,res){
+
+        let nombre = req.body.nombre;
+
+        Owner.findOne({where: {nombre: {[Op.like]: `%${nombre}%`}}}).then((owner)=>{            
+                        
+            if(owner){
+                res.render('owners/edit',{owner});
+            }else{
+                let errorMsg = "organizador no encontrado";                
+                res.render('owners/edit',{errorMsg});
+            }});                            
+    },
+    update: function(req,res){
+        
+        let owner = {
+            id: req.body.id,
+            nombre:req.body.description, 
+            website: req.body.website,
+        }
+
+        Owner.update({nombre: data.nombre,website: data.website},{
+            where: {
+                id: data.id,
+            }
+        }).then(function(response){
+            
+            owner.response = response;
+            
+            res.redirect('/owners/edit',{owner});
+        })    
+    }   
+
 }
--- routes/owner_routes.js
+++ routes/owner_routes.js
@@ -5,7 +5,11 @@ let ownerController = require('../controllers/owners');
 let router = express.Router();
 
 router.route('/owners/index').get(ownerController.index);
+router.route('/owners/search')
+    .get(ownerController.search)
+    .post(ownerController.find);
 router.route('/owners/new').get(ownerController.new);
+router.route('/owners/update').get(ownerController.update);
 router.route('/owners').post(ownerController.create);
 
 module.exports = router;
