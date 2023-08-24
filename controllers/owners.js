5022c91c22fc5a1a1bca734885c3b490e8c2f6a6
Carlos Barrionuevo0 <cbarrionuevo@fidelatam.com>
2023-08-24 20:49:33 -0300

    CRUD Owners

--- controllers/owners.js
+++ controllers/owners.js
@@ -3,64 +3,72 @@ const { Op } = require('sequelize');
 const Owner = require('../models').Owner;
 
 module.exports = {
-    index: function(req,res){
-        Owner.findAll().then((owners)=>{
-            console.log("La cantidad de owners es: ",owners.length);
-            res.render('owners/index',{owners});
+    index: function (req, res) {
+        Owner.findAll().then((owners) => {
+            console.log("La cantidad de owners es: ", owners.length);
+            res.render('owners/index', { owners });
         })
     },
-    new: function(req,res){
-        console.log("entra");
+    new: function (req, res) {        
         res.render('owners/new');
     },
-    create: function(req,res){
+    create: function (req, res) {
         let data = {
             nombre: req.body.nombre,
             website: req.body.website
         }
 
-        Owner.create(data).then(result =>{
+        Owner.create(data).then(result => {
             //Si sale ok mostramos un json
             res.json(result);
-        }).catch(err =>{
+        }).catch(err => {
             //Si hay error lo mostramos
             res.json(err);
         });
     },
-    search: function(req,res){
+    search: function (req, res) {
         res.render('owners/search');
     },
-    find: function(req,res){
+    update: function (req, res) {
+                       
+        console.log("id owner: ",req.params.id);
+
+        const own = Owner.findByPk(req.params.id).then(
+            (owner) =>{                
+                owner.nombre = req.body.nombre;
+                owner.website = req.body.website;                
+                owner.save().then(()=>{
+                    owner.updateStatus = true;
+                    res.render('owners/edit', {owner});
+                })
+            }
+        )                
+    },
+    destroy: function(req,res){
+        console.log("Entra al eliminar owner");
+        Owner.destroy({
+            where:{
+                id:req.params.id
+            }
+        }).then((result)=>{
+            res.send('Owner eliminado correctamente');
+        })
+    },
+    show: function (req, res) {
 
         let nombre = req.body.nombre;
 
-        Owner.findOne({where: {nombre: {[Op.like]: `%${nombre}%`}}}).then((owner)=>{            
-                        
-            if(owner){
-                res.render('owners/edit',{owner});
-            }else{
-                let errorMsg = "organizador no encontrado";                
-                res.render('owners/edit',{errorMsg});
-            }});                            
-    },
-    update: function(req,res){
-        
-        let owner = {
-            id: req.body.id,
-            nombre:req.body.description, 
-            website: req.body.website,
-        }
+        console.log("Nombre: ",nombre);
 
-        Owner.update({nombre: data.nombre,website: data.website},{
-            where: {
-                id: data.id,
+        Owner.findOne({ where: { nombre: { [Op.like]: `%${nombre}%` } } }).then((owner) => {
+                
+            if (owner) {                
+                res.render('owners/edit', {owner});
+            } else {
+                res.render('owners/search');
             }
-        }).then(function(response){
-            
-            owner.response = response;
-            
-            res.redirect('/owners/edit',{owner});
-        })    
-    }   
+        });    
+    },
+   
 
 }
--- routes/owner_routes.js
+++ routes/owner_routes.js
@@ -6,10 +6,13 @@ let router = express.Router();
 
 router.route('/owners/index').get(ownerController.index);
 router.route('/owners/search')
-    .get(ownerController.search)
-    .post(ownerController.find);
+    .get(ownerController.search);
+router.route('/owners/show')
+    .post(ownerController.show);
 router.route('/owners/new').get(ownerController.new);
-router.route('/owners/update').get(ownerController.update);
+router.route('/owners/:id')    
+    .put(ownerController.update)
+    .delete(ownerController.destroy)
 router.route('/owners').post(ownerController.create);
 
 module.exports = router;
